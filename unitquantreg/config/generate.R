66dda2dfcc411bd27db9400129f7613233232d2b
AndrMenezes <andrefelipemaringa@gmail.com>
2023-04-20 11:34:53 +0100

    update quantile regression presenatation

--- unitquantreg/config/generate.R
+++ unitquantreg/config/generate.R
@@ -5,4 +5,4 @@ unlink("index_files", recursive = TRUE)
 rmarkdown::render(input = "index.Rmd",
                   output_file = "index.html",
                   clean = TRUE)
-renderthis::to_pdf("index.html")
+# renderthis::to_pdf("index.html")
